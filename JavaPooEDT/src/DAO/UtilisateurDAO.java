6257324beea8ce3594fcdc9e658bed755f78f623
paulsenard <47627695+paulsenard@users.noreply.github.com>
2020-06-04 17:00:15 +0200

    Modif de UtilisateurDAO pour la page de connexion

--- JavaPooEDT/src/DAO/UtilisateurDAO.java
+++ JavaPooEDT/src/DAO/UtilisateurDAO.java
@@ -19,19 +19,38 @@ public class UtilisateurDAO extends DAO<Utilisateur>{
     public UtilisateurDAO(Connexion connect) {
         super(connect);
     }
+
     
     @Override
-    public Utilisateur find(int id){
+    public Utilisateur find(int id) {
         Utilisateur utilisateur = new Utilisateur();
         
         try {
-            ResultSet rset = con.getStmt().executeQuery("select * from Utilisateur where ID_Utilisateur = " + id);
+            ResultSet rset = con.getStmt().executeQuery("select * from utilisateur where ID_Utilisateur = " + id);
             if(rset.first()){
-                utilisateur = new Utilisateur(id, rset.getString("email"), rset.getString("pwd"), rset.getString("nom"), rset.getString("prenom"), rset.getInt("droit"));
+                utilisateur = new Utilisateur(id,rset.getString("email"), rset.getString("pwd"), rset.getString("nom"), rset.getString("prenom"), rset.getInt("droit"));
             }
         } catch (SQLException ex) {
         }
         
         return utilisateur;
     }
+    
+    public Utilisateur findLogin(String email, String pwd){
+        Utilisateur utilisateur = new Utilisateur();
+  
+        try {
+            ResultSet rset = con.getStmt().executeQuery("SELECT * FROM utilisateur WHERE email = '" + email +"' AND pwd = '" + pwd + "'"); 
+            if(rset.first()){
+
+                utilisateur = new Utilisateur(rset.getInt("ID_Utilisateur"), email, pwd, rset.getString("nom"), rset.getString("prenom"), rset.getInt("droit"));
+
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        
+        return utilisateur;
+    }
+    
 }
