e21581da9b5e9facc15f9b9eb56c88e4f21c48e8
paulsenard <47627695+paulsenard@users.noreply.github.com>
2020-06-07 16:43:38 +0200

    ajout seance à tester

--- JavaPooEDT/src/DAO/SeanceDAO.java
+++ JavaPooEDT/src/DAO/SeanceDAO.java
@@ -10,6 +10,10 @@ import Controlleur.Connexion;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import model.Seance;
@@ -19,6 +23,7 @@ import model.Seance;
  * @author victo
  */
 public class SeanceDAO extends DAO<Seance>{
+    private final Calendar calendar = Calendar.getInstance();
     
     public SeanceDAO(Connexion connect) {
         super(connect);
@@ -39,5 +44,310 @@ public class SeanceDAO extends DAO<Seance>{
         
         return seance;
     }
+    
+    public boolean ajouter(long date, double duree, int etat, int ID_Cours, int ID_Type, ArrayList<Integer> ID_Prof, ArrayList<Integer> ID_Groupe
+    , ArrayList<Integer> ID_Salle){
+        Seance seance = new Seance();
+        int ID_Seance = 0;
+        int verif = 0;
+        
+        try {
+            ///////////VERIFICATION DISPONIBILITE DES PROF SUR LE CRENEAU/////////
+            for(int i = 0; i < ID_Prof.size();i++){
+                 ResultSet rset_cours = con.getStmt().executeQuery("SELECT * "
+                         + "from seance_enseignants where ID_Enseignant = " + ID_Prof.get(i));
+                 while(rset_cours.next()){
+                     ResultSet rset = con.getStmt().executeQuery("SELECT * "
+                         + "from seance where ID_Seance = " + rset_cours.getInt("ID_Seance"));
+                     
+                     Timestamp time = new Timestamp(rset.getInt("date"));
+            
+                    Date t = new Date(time.getTime());
+                    calendar.setTime(t);
+                    int heure_f = 0;
+                    int duree_heure = (int) Math.floor(rset.getInt("duree"));
+                    int duree_minute = rset.getInt("duree") - duree_heure;
+                    int jour = calendar.get(Calendar.DAY_OF_MONTH);
+                    int mois = calendar.get(Calendar.MONTH);
+                    int heure_d = calendar.get(Calendar.HOUR_OF_DAY);
+                    int minute_d = calendar.get(Calendar.MINUTE);
+                    int debut = heure_d*100 + minute_d;
+                    int minute_f = minute_d + (duree_minute*60);
+                    if(minute_f>59){
+                        minute_f = minute_f - 60;
+                        heure_f = heure_d + duree_heure + 1;
+                    }else{
+                        heure_f = (int) (calendar.get(Calendar.HOUR_OF_DAY) + duree_heure);
+                    }
+                    int fin = heure_f*100 + minute_f;
+                    
+                    Timestamp time2 = new Timestamp(date);
+            
+                    Date t2 = new Date(time.getTime());
+                    calendar.setTime(t2);
+                    int heure_f2 = 0;
+                    int duree_heure2 = (int) Math.floor(duree);
+                    double duree_minute2 = duree - duree_heure2;
+                    int jour2 = calendar.get(Calendar.DAY_OF_MONTH);
+                    int mois2 = calendar.get(Calendar.MONTH);
+                    int heure_d2 = calendar.get(Calendar.HOUR_OF_DAY);
+                    int minute_d2 = calendar.get(Calendar.MINUTE);
+                    int debut2 = heure_d2*100 + minute_d2;
+                    int minute_f2 = minute_d2 + ((int)duree_minute2*60);
+                    if(minute_f2>59){
+                        minute_f2 = minute_f2 - 60;
+                        heure_f2 = heure_d2 + duree_heure2 + 1;
+                    }else{
+                        heure_f2 = (int) (calendar.get(Calendar.HOUR_OF_DAY) + duree_heure2);
+                    }
+                    int fin2 = heure_f2*100 + minute_f2;
+                    
+                    if(jour == jour2 && mois == mois2){
+                        if((debut2>debut && debut2<fin) || (fin2>debut && fin2<fin)){
+                            if (rset.getInt("etat") != 1){
+                                verif ++;
+                            }
+                        }
+                    }
+                    
+                 }
+            }
+            
+            ///////////VERIFICATION DISPONIBILITE DES GROUPES SUR LE CRENEAU/////////
+            for(int i = 0; i < ID_Groupe.size();i++){
+                 ResultSet rset_groupe = con.getStmt().executeQuery("SELECT * "
+                         + "from seance_groupe where ID_Groupe = " + ID_Groupe.get(i));
+                 while(rset_groupe.next()){
+                     ResultSet rset = con.getStmt().executeQuery("SELECT * "
+                         + "from seance where ID_Seance = " + rset_groupe.getInt("ID_Seance"));
+                     
+                     Timestamp time = new Timestamp(rset.getInt("date"));
+            
+                    Date t = new Date(time.getTime());
+                    calendar.setTime(t);
+                    int heure_f = 0;
+                    int duree_heure = (int) Math.floor(rset.getInt("duree"));
+                    int duree_minute = rset.getInt("duree") - duree_heure;
+                    int jour = calendar.get(Calendar.DAY_OF_MONTH);
+                    int mois = calendar.get(Calendar.MONTH);
+                    int heure_d = calendar.get(Calendar.HOUR_OF_DAY);
+                    int minute_d = calendar.get(Calendar.MINUTE);
+                    int debut = heure_d*100 + minute_d;
+                    int minute_f = minute_d + (duree_minute*60);
+                    if(minute_f>59){
+                        minute_f = minute_f - 60;
+                        heure_f = heure_d + duree_heure + 1;
+                    }else{
+                        heure_f = (int) (calendar.get(Calendar.HOUR_OF_DAY) + duree_heure);
+                    }
+                    int fin = heure_f*100 + minute_f;
+                    
+                    Timestamp time2 = new Timestamp(date);
+            
+                    Date t2 = new Date(time.getTime());
+                    calendar.setTime(t2);
+                    int heure_f2 = 0;
+                    int duree_heure2 = (int) Math.floor(duree);
+                    double duree_minute2 = duree - duree_heure2;
+                    int jour2 = calendar.get(Calendar.DAY_OF_MONTH);
+                    int mois2 = calendar.get(Calendar.MONTH);
+                    int heure_d2 = calendar.get(Calendar.HOUR_OF_DAY);
+                    int minute_d2 = calendar.get(Calendar.MINUTE);
+                    int debut2 = heure_d2*100 + minute_d2;
+                    int minute_f2 = minute_d2 + ((int)duree_minute2*60);
+                    if(minute_f2>59){
+                        minute_f2 = minute_f2 - 60;
+                        heure_f2 = heure_d2 + duree_heure2 + 1;
+                    }else{
+                        heure_f2 = (int) (calendar.get(Calendar.HOUR_OF_DAY) + duree_heure2);
+                    }
+                    int fin2 = heure_f2*100 + minute_f2;
+                    
+                    if(jour == jour2 && mois == mois2){
+                        if((debut2>debut && debut2<fin) || (fin2>debut && fin2<fin)){
+                            if (rset.getInt("etat") != 1){
+                                verif ++;
+                            }
+                        }
+                    }
+                    
+                 }
+            }
+            
+             ///////////VERIFICATION DISPONIBILITE DES SALLES SUR LE CRENEAU/////////
+             for(int i = 0; i < ID_Salle.size();i++){
+                 ResultSet rset_salle = con.getStmt().executeQuery("SELECT * "
+                         + "from seance_salles where ID_Salle = " + ID_Salle.get(i));
+                 while(rset_salle.next()){
+                     ResultSet rset = con.getStmt().executeQuery("SELECT * "
+                         + "from seance where ID_Seance = " + rset_salle.getInt("ID_Seance"));
+                     
+                     Timestamp time = new Timestamp(rset.getInt("date"));
+            
+                    Date t = new Date(time.getTime());
+                    calendar.setTime(t);
+                    int heure_f = 0;
+                    int duree_heure = (int) Math.floor(rset.getInt("duree"));
+                    int duree_minute = rset.getInt("duree") - duree_heure;
+                    int jour = calendar.get(Calendar.DAY_OF_MONTH);
+                    int mois = calendar.get(Calendar.MONTH);
+                    int heure_d = calendar.get(Calendar.HOUR_OF_DAY);
+                    int minute_d = calendar.get(Calendar.MINUTE);
+                    int debut = heure_d*100 + minute_d;
+                    int minute_f = minute_d + (duree_minute*60);
+                    if(minute_f>59){
+                        minute_f = minute_f - 60;
+                        heure_f = heure_d + duree_heure + 1;
+                    }else{
+                        heure_f = (int) (calendar.get(Calendar.HOUR_OF_DAY) + duree_heure);
+                    }
+                    int fin = heure_f*100 + minute_f;
+                    
+                    Timestamp time2 = new Timestamp(date);
+            
+                    Date t2 = new Date(time.getTime());
+                    calendar.setTime(t2);
+                    int heure_f2 = 0;
+                    int duree_heure2 = (int) Math.floor(duree);
+                    double duree_minute2 = duree - duree_heure2;
+                    int jour2 = calendar.get(Calendar.DAY_OF_MONTH);
+                    int mois2 = calendar.get(Calendar.MONTH);
+                    int heure_d2 = calendar.get(Calendar.HOUR_OF_DAY);
+                    int minute_d2 = calendar.get(Calendar.MINUTE);
+                    int debut2 = heure_d2*100 + minute_d2;
+                    int minute_f2 = minute_d2 + ((int)duree_minute2*60);
+                    if(minute_f2>59){
+                        minute_f2 = minute_f2 - 60;
+                        heure_f2 = heure_d2 + duree_heure2 + 1;
+                    }else{
+                        heure_f2 = (int) (calendar.get(Calendar.HOUR_OF_DAY) + duree_heure2);
+                    }
+                    int fin2 = heure_f2*100 + minute_f2;
+                    
+                    if(jour == jour2 && mois == mois2){
+                        if((debut2>debut && debut2<fin) || (fin2>debut && fin2<fin)){
+                            if (rset.getInt("etat") != 1){
+                                verif ++;
+                            }
+                        }
+                    }
+                    
+                 }
+            }
+            
+             ///////////VERIFICATION DE lA CAPACITE DES SALLES SUR LE CRENEAU/////////
+             int capacite_salle = 0;
+             for(int i = 0; i < ID_Salle.size();i++){
+                 ResultSet rset_salle = con.getStmt().executeQuery("SELECT * "
+                         + "from salle where ID_Salle = " + ID_Salle.get(i));
+                 if(rset_salle.first()){
+                     
+                     capacite_salle = capacite_salle + rset_salle.getInt("capcite");
+                      
+                 }
+            }
+             int capacite_groupe = 0;
+             for(int i = 0; i < ID_Groupe.size();i++){
+                 ResultSet rset_groupe = con.getStmt().executeQuery("SELECT COUNT (*) "
+                         + "from etudiant where ID_Groupe = " + ID_Groupe.get(i));
+                     
+                     capacite_groupe = capacite_groupe + rset_groupe.getInt("capcite");
+                      
+
+            }
+            
+            if(capacite_groupe > capacite_salle){
+                verif ++;
+            }
+            
+            if(etat == 1){
+                verif ++;
+            }
+            
+            if(verif == 0){
+                con.getStmt().executeUpdate("INSERT INTO seance (date, duree, etat, ID_Cours, ID_Type) VALUES ("+duree+","
+                    +etat+","+ID_Cours+","+ID_Type+");");
+                ResultSet rset_id = con.getStmt().executeQuery("SELECT * " +
+                    "FROM senace WHERE ID_Seance" +
+                    "(SELECT MAX(ID_Seance) FROM seance);");
+                int id_seance = rset_id.getInt("ID_Seance");
+                for(int i = 0; i < ID_Prof.size();i++){
+                    con.getStmt().executeUpdate("INSERT INTO seance_enseignants (ID_Seance, ID_Enseignant) VALUES ("+id_seance+","
+                    +ID_Prof.get(i)+";");
+                }
+                for(int i = 0; i < ID_Groupe.size();i++){
+                    con.getStmt().executeUpdate("INSERT INTO seance_groupe (ID_Seance, ID_Groupe) VALUES ("+id_seance+","
+                    +ID_Groupe.get(i)+";");
+                }
+                for(int i = 0; i < ID_Salle.size();i++){
+                    con.getStmt().executeUpdate("INSERT INTO seance_salles (ID_Seance, ID_Salle) VALUES ("+id_seance+","
+                    +ID_Salle.get(i)+";");
+                }
+                
+            }
+                         
+            
+        } catch (SQLException ex) {
+        }
+        if(verif == 0){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    
+   
+    public void modifier(int id){
+        Seance seance = new Seance();
+        
+        try {
+            ResultSet rset = con.getStmt().executeQuery("select * from seance where ID_Seance = " + id);
+            if(rset.first()){
+                seance = new Seance(id, rset.getInt("semaine"), rset.getLong("date"), rset.getFloat("duree"),
+                        rset.getInt("etat"), rset.getInt("ID_Cours"), rset.getInt("ID_Type"));
+            }
+        } catch (SQLException ex) {
+        }
+        
+      
+    }
+    
+    public void annuler(int id){
+        
+        
+        try {
+            con.getStmt().executeUpdate("UPDATE seance SET etat = '1' WHERE ID_Seance = " + id);
+            
+        } catch (SQLException ex) {
+        }
+        
+      
+    }
+    
+    public void valider(int id){
+        
+        
+        try {
+            con.getStmt().executeUpdate("UPDATE seance SET etat = '0' WHERE ID_Seance = " + id);
+            
+        } catch (SQLException ex) {
+        }
+        
+      
+    }
+    
+
+    public void supprimer(int id){
+        
+        
+        try {
+            con.getStmt().executeUpdate("DELETE FROM seance WHERE ID_Seance = " + id);
+            
+        } catch (SQLException ex) {
+        }      
+       
+    }
+    
+    
     
 }
